14057094e8b5970a983dd48bc510602e14ddc656
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-02-27 09:30:07 -0500

    rm certfile stuff

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,12 +3,7 @@ from os import environ
 PORT=environ.pop('PORT')
 WORKERS=environ.pop('WORKERS')
 SITENAME=environ.pop('SITENAME')
-CERTPATH=environ.pop('CERTPATH')
 
 bind='0.0.0.0:{}'.format(PORT)
 workers=int(WORKERS)
-if CERTPATH != 'default':
-    certfile='{}/cert.pem'.format(CERTPATH)
-    keyfile='{}/privkey.pem'.format(CERTPATH)
-    ca_certs='{}/chain.pem'.format(CERTPATH)
 application='web:app'
