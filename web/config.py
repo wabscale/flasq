d425abba0f367f528b05669becb16c0096de1c5c
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-02-27 09:31:14 -0500

    auto debug config

--- web/config.py
+++ web/config.py
@@ -2,5 +2,11 @@
 import os
 class Config():
     SECRET_KEY = os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/db.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///.data/db.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DOMAIN='https://f.bigj.info'
+
+    def __init__(self):
+        if 'dev.py' in sys.argv:
+            self.SECRET_KEY='DEBUG'
+            self.DOMAIN='http://localhost:5000'
