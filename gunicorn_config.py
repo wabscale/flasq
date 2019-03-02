c5a718b85fa7dfe3d2e30dfaef8b14e628f637d3
John McCann Cunniff Jr <johncunniff1248@gmail.com>
2019-03-01 20:02:17 -0500

    add logging and preload to gunicorn config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,9 +1,10 @@
 from os import environ
 
-PORT=environ.pop('PORT')
-WORKERS=environ.pop('WORKERS')
-SITENAME=environ.pop('SITENAME')
+PORT=environ.pop('PORT') if 'PORT' in environ else '80'
+WORKERS=environ.pop('WORKERS') if 'WORKERS' in environ else '2'
+SITENAME=environ.pop('SITENAME') if 'SITENAME' in environ else 'DEFAULT'
 
 bind='0.0.0.0:{}'.format(PORT)
 workers=int(WORKERS)
-application='web:app'
+errorlog='web/data/error.log'
+preload_app=True
